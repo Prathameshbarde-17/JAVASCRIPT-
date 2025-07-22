0ccd619cf8c99a68369048aba1e4fed4886eda37
Prathamesh Barde <167535391+Prathameshbarde-17@users.noreply.github.com>
2025-07-23 01:09:36 +0530

    Add files via upload
    Introduction JS

---
+++ FirstCode.js
@@ -0,0 +1,40 @@
+fullName = "Prathamesh Barde..";
+age = 22;
+console.log(fullName + age);
+console.log("Lets Start Javascript0");
+console.log("Jay Bhavani, Jai Shivaji");
+
+//Variable--->
+//There are three type of Variable-->1.Var
+//                                   2.let
+//                                   3.const
+// Q1
+//var -->It is global variable.
+var rollNo = 25;
+var rollNo = 30;
+var rollNo = 40;
+var Age = 24;
+console.log("Data:" + rollNo + Age);
+
+//let -->It is block variable
+let uAge = 25;
+uAge = 10;
+console.log("Age" + uAge);
+
+let Student = {
+  fN: "Prathamesh Barde",
+  age: 22,
+};
+console.log(Student);
+
+//const--> It is block variable..
+const PI = 3.14;
+console.log("PI Formula:" + PI);
+
+const Std = {
+  emName: "XYZ",
+  emNo: 1771,
+  emailEm: "abc@123",
+};
+console.log(Std);
+console.log(Std["emName"]);
---
+++ firstEx.js
@@ -0,0 +1,10 @@
+//Create a const object called “product” to store information shown in the picture.
+const product = {
+  imgName: "Parker Jotter Standard Pen",
+  Colour: "Black",
+  Price: 270,
+  Discount: "50%",
+  isBollen: false,
+};
+//product = "AVB";
+console.log(product);
