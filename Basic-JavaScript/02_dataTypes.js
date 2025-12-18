2043f529c8760b84b5333f26cce31db9b48d2853
Prathamesh Barde <167535391+Prathameshbarde-17@users.noreply.github.com>
2025-12-18 13:37:53 +0530

    Update variable names and improve comments in 02_dataTypes.js

--- Basic-JavaScript/02_dataTypes.js
+++ Basic-JavaScript/02_dataTypes.js
@@ -1,3 +1,16 @@
+
+//Data Types
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+// object
+
+
 "use strict"; // treat all JS code as newer version
 
 // alert( 3 + 3) // we are using nodejs, not browser
@@ -6,10 +19,10 @@ console.log(3
     +
      3) // code readability should be high
 
-console.log("Hitesh")
+console.log("PRATHAMESH")
 
 
-let name = "hitesh"
+let name = "PRANITA"
 let age = 18
 let isLoggedIn = false
 let state;
@@ -21,9 +34,6 @@ let state;
 // null => standalone value
 // undefined => 
 // symbol => unique
-
-
-// object
 
 console.log(typeof undefined); // undefined
 console.log(typeof null); // object
