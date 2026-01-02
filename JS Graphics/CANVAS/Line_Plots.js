989864c3cdb1f969f1fbcc92a908b20162938de4
Prathamesh Barde <167535391+Prathameshbarde-17@users.noreply.github.com>
2026-01-02 22:20:54 +0530

    ADD js FIle

---
+++ JS Graphics/CANVAS/Line_Plots.js
@@ -0,0 +1,20 @@
+const canvas = document.getElementById("canvas");
+
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "#FF0000";
+canvas.height = canvas.width;
+ctx.transform(1, 0, 0, -1, 0, canvas.height);
+
+let xMax = canvas.height;
+let slope = 1.2;
+let intercept = 70;
+
+ctx.moveTo(0, intercept);
+ctx.lineTo(xMax, f(xMax));
+ctx.strokeStyle = "black";
+ctx.stroke();
+
+function f(x) {
+  return x * slope + intercept;
+}
